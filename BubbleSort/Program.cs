40c7353537777c10288ce5a2212d8254ebd4b0ff
Dzevad Trokic <d.trokic@cp-pro.de>
2022-06-13 14:10:57 +0200

    Projektdateien hinzufügen.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+public class Programm
+{
+    static void Main(string[] args)
+    {
+        // Zu sortierende Array 
+        int[] arr = { 88, 55, 1, 6, 80, 99, 556, 700 };
+
+        // Hilfsvariable
+        int temp = 0;
+
+        // Durchlauf Länge des Arrays
+        for (int i = 0; i < arr.Length; i++)
+        {
+            // Durchlauf Inhalt des Arrays
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+
+                // Abfrage ob der erste Index größer ist ls der nächste im Array
+                if (arr[j] > arr[j + 1])
+                {
+                    // Zwischenspeichern in die Hilfsvariable und tauschen
+                    temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        // Augabe des Sortierten Arrays
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.ReadKey();
+    }
+}
