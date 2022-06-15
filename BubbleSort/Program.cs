88951b25b497c981d43b7e447f1a27359059b7c5
Dzevad Trokic <d.trokic@cp-pro.de>
2022-06-15 08:56:05 +0200

    Methoden hinzugefügt

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -5,19 +5,34 @@ public class Programm
     static void Main(string[] args)
     {
         // Zu sortierende Array 
-        int[] arr = { 88, 55, 1, 6, 80, 99, 556, 700,10 , 15 };
+        int[] arr = { 88, 55, 1, 6, 80, 99, 556, 700, 10, 15 };
 
         // Hilfsvariable
         int temp = 0;
 
-        // Durchlauf Länge des Arrays
+        temp = BubbleSort(arr, temp);
+        DruckArray(arr);
+    }
+
+    // Augabe des Sortierten Arrays
+    private static void DruckArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.ReadKey();
+    }
+
+    // Durchlauf Länge des Arrays
+    private static int BubbleSort(int[] arr, int temp)
+    {
         for (int i = 0; i < arr.Length; i++)
         {
             // Durchlauf Inhalt des Arrays
             for (int j = 0; j < arr.Length - 1; j++)
             {
-
-                // Abfrage ob der erste Index größer ist ls der nächste im Array
+                // Abfrage ob der erste Index größer ist als der nächste im Array
                 if (arr[j] > arr[j + 1])
                 {
                     // Zwischenspeichern in die Hilfsvariable und tauschen
@@ -27,11 +42,7 @@ public class Programm
                 }
             }
         }
-        // Augabe des Sortierten Arrays
-        for (int i = 0; i < arr.Length; i++)
-        {
-            Console.Write(arr[i] + " ");
-        }
-        Console.ReadKey();
+
+        return temp;
     }
 }
